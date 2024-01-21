ba5d551c5f32aadb056c5b8de84cf19d3f3daf3e
anseranser <serge.loie@yandex.ru>
2024-01-21 14:45:45 +0500

    initialize project

---
+++ app/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    application
+    idea
+    jacoco
+    checkstyle
+    id("io.freefair.lombok") version "8.4"
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
+    id("com.github.ben-manes.versions") version "0.50.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("org.sonarqube") version "4.4.1.3373"
+
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required = true
+        html.outputLocation = layout.buildDirectory.dir("jacocoHtml")
+    }
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "sergeloie_java-project-72")
+        property("sonar.organization", "sergeloie")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
---
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -0,0 +1,13 @@
+package hexlet.code;
+
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+//import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.Test;
+
+public final class AppTest {
+    public void dummyTest() {
+        assertTrue(true);
+    }
+}
